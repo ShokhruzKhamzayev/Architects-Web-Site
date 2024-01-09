222fbab040ed5c0366aff51c76dcd9053e7a0bb4
Shoxruz <forpk432@gmail.com>
2024-01-10 00:35:54 +0500

    finishing_ground_main_page

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -6,6 +6,8 @@ import 'swiper/css/bundle';
 
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import Image from 'next/image';
+import Link from 'next/link';
+import arrow_right from '../assets/arrow-right.svg'
 
 export default function Main() {
     return (
@@ -15,7 +17,7 @@ export default function Main() {
                 centeredSlides={true}
                 autoplay={{
                     delay: 2500,
-                    disableOnInteraction: false,
+                    disableOnInteraction: true,
                 }}
                 pagination={{
                     clickable: true,
@@ -25,32 +27,50 @@ export default function Main() {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <div className='flex justify-between items-center w-[80%]'>
+                    <div className='flex justify-between items-center w-[80%] flex-col lg:flex-row gap-[30px]'>
                         <div>
                             <h1 className='text-[#bdbdbd] text-[30px] md:text-[42px] lg:text-[62px] text-start leading-none'>PROJECT <span className='text-[#333333] font-[800]'>NURTOWN</span></h1>
                         </div>
-                        <div className="img-main">
-                            <Image src={content_main} alt='photo of skyscraper' className='max-w-[550px] min-h-[500px]' />
+                        <div className="img-main relative">
+                            <Image src={content_main} alt='photo of skyscraper' className='max-w-[550px] min-h-[200px] lg:min-h-[450px] custom-img' />
+                            <div className="float-screen-main absolute bottom-[10px] left-[50%] -translate-x-[50%] lg:-translate-x-0 lg:left-0 lg:bottom-0">
+                                <Link href={'/'} className='flex justify-center items-center bg-white min-w-[222px] h-[61px] gap-[10px]'>
+                                    <p className='text-[12px]'>ВЗГЛЯНУТЬ</p>
+                                    <Image src={arrow_right} alt='icon of arrow' className='w-[30px]' />
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='flex justify-between items-center w-[80%]'>
+                    <div className='flex justify-between items-center w-[80%] flex-col lg:flex-row gap-[30px]'>
                         <div>
                             <h1 className='text-[#bdbdbd] text-[30px] md:text-[42px] lg:text-[62px] text-start leading-none'>PROJECT <span className='text-[#333333] font-[800]'>NURTOWN</span></h1>
                         </div>
-                        <div className="img-main">
-                            <Image src={content_main} alt='photo of skyscraper' className='max-w-[550px] min-h-[500px]' />
+                        <div className="img-main relative">
+                            <Image src={content_main} alt='photo of skyscraper' className='max-w-[550px] min-h-[200px] lg:min-h-[450px] custom-img' />
+                            <div className="float-screen-main absolute bottom-[10px] left-[50%] -translate-x-[50%] lg:-translate-x-0 lg:left-0 lg:bottom-0">
+                                <Link href={'/'} className='flex justify-center items-center bg-white min-w-[222px] h-[61px] gap-[10px]'>
+                                    <p className='text-[12px]'>ВЗГЛЯНУТЬ</p>
+                                    <Image src={arrow_right} alt='icon of arrow' className='w-[30px]'/>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='flex justify-between items-center w-[80%]'>
+                    <div className='flex justify-between items-center w-[80%] flex-col lg:flex-row gap-[30px]'>
                         <div>
                             <h1 className='text-[#bdbdbd] text-[30px] md:text-[42px] lg:text-[62px] text-start leading-none'>PROJECT <span className='text-[#333333] font-[800]'>NURTOWN</span></h1>
                         </div>
-                        <div className="img-main">
-                            <Image src={content_main} alt='photo of skyscraper' className='max-w-[550px] min-h-[500px]' />
+                        <div className="img-main relative">
+                            <Image src={content_main} alt='photo of skyscraper' className='max-w-[550px] min-h-[200px] lg:min-h-[450px] custom-img' />
+                            <div className="float-screen-main absolute bottom-[10px] left-[50%] -translate-x-[50%] lg:-translate-x-0 lg:left-0 lg:bottom-0">
+                                <Link href={'/'} className='flex justify-center items-center bg-white min-w-[222px] h-[61px] gap-[10px]'>
+                                    <p className='text-[12px]'>ВЗГЛЯНУТЬ</p>
+                                    <Image src={arrow_right} alt='icon of arrow' className='w-[30px]' />
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </SwiperSlide>
